62695abbc519edefe03197400e7fe5fba70de415
Kibet Chirchir <langatchirchir@gmail.com>
2018-10-28 11:14:12 +0300

    separated the test to make test more effective

--- test_latin.py
+++ test_latin.py
@@ -1,22 +1,35 @@
 import  unittest
 from latin import latin
 
+letter = latin()
+
 
 class TestLatin(unittest.TestCase):
-    def test_convert(self):
-        letter = latin()
+    def test_consonants(self):
         self.assertEqual(letter.convert('dog'), 'ogday')
         self.assertEqual(letter.convert('will'), 'illway')
         self.assertEqual(letter.convert('category'), 'ategorycay')
+
+    def test_constants_2(self):
         self.assertEqual(letter.convert('chatter'), 'atterchay')
         self.assertEqual(letter.convert('trash'), 'ashtray')
+
+    def test_vowels(self):
         self.assertEqual(letter.convert('andela'), 'andelaway')
         self.assertEqual(letter.convert('electritian'), 'electritianway')
         self.assertEqual(letter.convert('esther'), 'estherway')
+
+    def test_numeric(self):
         self.assertEqual(letter.convert('K454dfdf'), 'word is invalid  contains numeric value')
+
+    def test_empty(self):
         self.assertEqual(letter.convert(''), 'empty word not allowed')
+
+    def test_letter(self):
         self.assertEqual(letter.convert('a'), 'letter not allowed')
         self.assertEqual(letter.convert('z'), 'letter not allowed')
+
+    def test_vowels_only(self):
         self.assertEqual(letter.convert('jdjfdf'), 'jdjfdf word cannot be changed')
 
 
